1fb8d8f360cc1c30c741f15404fe0786db87eea6
David Serrano <75829857+serranoio@users.noreply.github.com>
2024-08-31 21:29:13 -0700

    done

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,6 +56,7 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
+          routeBasePath: "/blog",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -125,7 +126,7 @@ const config: Config = {
             },
             {
               label: 'About',
-              to: '/about',
+              to: '/about/intro',
             },
             {
               label: 'GitHub',
